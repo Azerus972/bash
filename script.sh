abaac47a7829f4c8f80492131620954a16bf03d8
KevinMarous <kevin.marous@gmail.com>
2017-11-23 16:32:47 +0100

    script updated

--- script.sh
+++ script.sh
@@ -1,10 +1,88 @@
+
 echo " Initialisation du logiciel...."
 
-read -p " Bienvenu sur My Bash Instalator, que souhaitez-vous faire ?
 
-1 - installer Vagrant ?
-2 - Installer VirtualBox ?"
+echo "Bienvenu sur My Bash Instalator, que souhaitez-vous faire ?"
 
-if [ $Vagrant -eq 1 ] 
+echo -e "
+------------------------------------------------------------
+ 1 - Installer Vagrant ?
+------------------------------------------------------------
+"
+echo -e "
+------------------------------------------------------------
+ 2 - Installer VirtualBox ?
+ -----------------------------------------------------------
+ "
 
-then
+read -p " Votre choix : " reponse
+
+echo
+
+case $reponse in
+
+1)
+
+vagrant init;
+
+echo "
+------------------------------------------------------------
+Fichier crée / Si déjà existant aucun doublon ne sera crée
+------------------------------------------------------------
+";;
+
+2)
+
+sudo apt-get install virtualbox;
+
+echo "
+------------------------------------------------------------
+Virtualbox installé avec succès ! 
+------------------------------------------------------------
+";;
+
+esac
+
+echo  "
+------------------------------------------------------------
+Compris Quelle box choisissez vous ?
+------------------------------------------------------------
+"
+
+echo e- "
+------------------------------------------------------------
+ 1 - Xenial64
+ ------------------------------------------------------------
+ "
+
+echo e- " 
+------------------------------------------------------------
+2 - Trusty64
+------------------------------------------------------------
+"
+
+read -p " Votre choix : " tellya
+
+case $tella in
+
+1)
+
+vagrant init ubuntu/xenial64;
+
+echo "
+------------------------------------------------------------
+Xenial64 configuré
+------------------------------------------------------------
+";;
+
+2)
+
+vagrant init ubuntu/trusty64;
+
+echo "
+------------------------------------------------------------
+Trusty64 configuré
+------------------------------------------------------------
+";;
+
+esac
