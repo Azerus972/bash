9365a316601a52b16cb89a11b2893b83859b9a84
KevinMarous <kevin.marous@gmail.com>
2017-11-23 14:44:56 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+echo " Initialisation du logiciel...."
+
+read -p " Bienvenu sur My Bash Instalator, que souhaitez-vous faire ?
+
+1 - installer Vagrant ?
+2 - Installer VirtualBox ?"
+
+if [ $Vagrant -eq 1 ] 
+
+then
